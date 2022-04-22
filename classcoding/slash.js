9625762521007049c328567e3ba524abc7b5a264
Micah Norrington <manorrington@gmail.com>
2022-04-22 13:45:12 -0400

    added files

---
+++ classcoding/slash.js
@@ -0,0 +1,24 @@
+let firstName = `Micah`
+console.log(firstName)
+
+let string = [`This`, `is`, `a`, `string`, `yes`]
+let number = [1, 2, 3, 4, 5]
+let boolean = [1 > 2, 2 < 3, 3 == 3, 4 >= 5, 5 <= 6]
+ 
+if (element === string) {
+
+}
+
+
+
+
+function (element) {
+    elementTags
+}
+element
+
+let elements = {
+    string:[ `This is a string`],
+    number: [1, 2, 3, 4, 5],
+    boolean: [1 > 2, 2 < 3, 3 == 3, 4 >= 5, 5 <= 6]
+}
