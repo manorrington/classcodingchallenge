9690edcf441b2822d8f8b56adc73b1aa4ed9dd9d
Micah Norrington <manorrington@gmail.com>
2022-04-22 13:58:45 -0400

    made some changes

--- classcoding/slash.js
+++ classcoding/slash.js
@@ -1,24 +1,9 @@
-let firstName = `Micah`
-console.log(firstName)
-
-let string = [`This`, `is`, `a`, `string`, `yes`]
-let number = [1, 2, 3, 4, 5]
-let boolean = [1 > 2, 2 < 3, 3 == 3, 4 >= 5, 5 <= 6]
- 
-if (element === string) {
-
-}
-
-
-
-
-function (element) {
-    elementTags
-}
-element
-
 let elements = {
-    string:[ `This is a string`],
+    string:[ `Hi`, `Hello`, `Hola`, `Good morning`, `Good night`],
     number: [1, 2, 3, 4, 5],
     boolean: [1 > 2, 2 < 3, 3 == 3, 4 >= 5, 5 <= 6]
 }
+
+console.log(elements.string[0])
+console.log(elements.number[3])
+console.log(elements.boolean[1])
